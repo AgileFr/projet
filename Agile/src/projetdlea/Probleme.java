910cd697da113c6f48ef0039cee11c4af81e728c
VincentDEJ <dejonckheere.vincent@gmail.com>
2014-03-04 22:11:52 -0500

    Congé maladie OK

--- Agile/src/projetdlea/Probleme.java
+++ Agile/src/projetdlea/Probleme.java
@@ -25,25 +25,42 @@ public class Probleme {
         String jourSemaine = "jour";
         boolean jourDeLaSemaine = true;
         while(debut<=fin){
+            boolean malade = false;
             int cptMinutesJourBureau = 0;
             int cptMinutesJourTeletravail = 0;
             JSONArray test = (JSONArray) JSONSerializer.toJSON(root.getString(jourSemaine+debut));
             int docCount = test.size();
             for(int j=0; j<docCount; j++){
                 JSONObject document = test.getJSONObject(j);
-                numProjet = Integer.parseInt(root.getString("projet"));
+                numProjet = Integer.parseInt(document.getString("projet"));
                 minutes = Integer.parseInt(document.getString("minutes"));
                 if(projetBureau(numProjet)){
-                    ajoutMinutesBureau(minutes);
-                    if(jourDeLaSemaine) cptMinutesJourBureau += minutes;
+                     if (malade) json += " \n \" L'employé a travaillé " + minutes + " minutes pour le projet " + numProjet + " au bureau alors qu'il était en congé maladie le jour "+ debut + "\", ";
+                     else{
+                        ajoutMinutesBureau(minutes);
+                        if(jourDeLaSemaine) cptMinutesJourBureau += minutes;
+                     }
                 }
-                else if ()
-               else {
-                    ajoutMinutesTeletravail(minutes);
-                    cptMinutesJourTeletravail += minutes;
+                else if (projetTeletravail(numProjet)){
+                    if (malade) json += " \n \" L'employé a travaillé en télétravail " + minutes + " minutes pour le projet " + numProjet + " alors qu'il était en congé maladie le jour "+ debut + "\", ";
+                    else{
+                        ajoutMinutesTeletravail(minutes);
+                        cptMinutesJourTeletravail += minutes;
+                    }
+                }
+                else if (congeMaladie(numProjet)){ 
+                    if(jourDeLaSemaine){
+                        malade = true;
+                        if (minutes != 420){
+                        json += " \n \" L'employé a chargé " + minutes + " minutes au lieu de 420 lors d'un congé maladie le jour"+ debut + "\", ";
+                        } 
+                        ajoutMinutesBureau(minutes);
+                        cptMinutesJourBureau += minutes;
+                    }
+                    else json += " \n \" L'employé a pris un congé maladie le "+ jourSemaine + "\", ";
                 }
            }
-           if(jourDeLaSemaine == true && !respectMaxJour(cptMinutesJourBureau+cptMinutesJourTeletravail)) json += " \n \" L'employé a travaillé plus de 24 heures "+ debut + "\", ";
+           if(respectMaxJour(cptMinutesJourBureau+cptMinutesJourTeletravail)) json += " \n \" L'employé a travaillé plus de 24 heures "+ debut + "\", ";
            if(jourDeLaSemaine == true && respectMinutesMiniBureau(cptMinutesJourBureau)) json += " \n \" L'employé n'à travaillé le nombre d'heures minimal pour le jour "+ debut + "\", ";
            if(debut == 5){debut = 1; fin = 2; jourSemaine="weekend"; jourDeLaSemaine = false;}
            debut++;
